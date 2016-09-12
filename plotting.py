6ba2026b6efba4e340aebb0d972ccfa5e3744734
Bill Lotter <blotter23@gmail.com>
2016-09-12 15:40:30 -0400

    Prettier compare_images

--- plotting.py
+++ plotting.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.gridspec as gridspec
+import pdb
 
 def plot_training_error(train_err = None, val_err = None, run_name = None, out_file = None):
     plt.figure()
@@ -35,31 +37,35 @@ def plot_training_error(train_err = None, val_err = None, run_name = None, out_f
     plt.close()
 
 
-def compare_images(x, x_hat, save_names, titles, cmap='Greys_r', vmin=None, vmax=None):
+def compare_images(x, x_hat, save_names, titles=None, cmap='Greys_r', vmin=None, vmax=None):
     '''
     x is like (n_samples, nt, n_channels, nx, ny)
     '''
-    if x.shape[2] == 1:
-        is_color = False
-    else:
-        is_color = True
+    is_color = x.shape[2] != 1
     nt = x.shape[1]
+    aspect_ratio = float(x_hat.shape[3]) / x_hat.shape[4]
+    plt.figure(figsize = (nt, 2*aspect_ratio))
+    gs = gridspec.GridSpec(2, nt)
+    gs.update(wspace=0., hspace=0.)
     for i in range(x.shape[0]):
-        for t in range(x.shape[1]):
-            plt.subplot(2, nt, t+1)
+        for t in range(nt):
+            plt.subplot(gs[t])
             if is_color:
                 plt.imshow(np.transpose(x[i,t], (1, 2, 0)), interpolation='none')
             else:
                 plt.imshow(x[i,t,0], cmap=cmap, vmin=vmin, vmax=vmax, interpolation='none')
-            plt.axis('off')
+            plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
             if t==0:
-                plt.title(titles[i])
+                if titles is not None: plt.title(titles[i])
+                plt.ylabel('Actual', fontsize=10)
 
-            plt.subplot(2, nt, t+nt+1)
+            plt.subplot(gs[t + nt])
             if is_color:
                 plt.imshow(np.transpose(x_hat[i,t], (1, 2, 0)), interpolation='none')
             else:
                 plt.imshow(x_hat[i,t,0], cmap=cmap, vmin=vmin, vmax=vmax, interpolation='none')
-            plt.axis('off')
+            plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
+            if t==0: plt.ylabel('Predicted', fontsize=10)
 
-            plt.savefig(save_names[i])
+        plt.savefig(save_names[i])
+        plt.clf()
