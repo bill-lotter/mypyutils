487b9f2cd048682c69174e9f4a8285708f74b6a7
Bill Lotter <blotter23@gmail.com>
2018-01-05 15:18:17 -0500

    Added roc plots

--- plotting.py
+++ plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.gridspec as gridspec
 import pdb
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
 
 def plot_training_error(train_err = None, val_err = None, run_name = None, out_file = None):
     plt.figure()
@@ -69,3 +70,58 @@ def compare_images(x, x_hat, save_names, titles=None, cmap='Greys_r', vmin=None,
 
         plt.savefig(save_names[i])
         plt.clf()
+
+
+def plot_ranked_ims(y, y_hat, x, n_plot, save_dir):
+    idx = np.argsort(y_hat.flatten())
+    y_hat = y_hat[idx]
+    y = y[idx]
+    x = x[idx]
+    n_plot = min(n_plot, len(y)/2)
+
+    plt.figure()
+    for i in range(-n_plot, n_plot):
+        plt.imshow(x[i,0], cmap="Greys_r", interpolation='none')
+        plt.title('rank='+str(i)+' y='+str(y[i])+' yhat='+str(y_hat[i]))
+        plt.axis('off')
+        plt.savefig(save_dir+'plot_'+str(i)+'.png')
+
+    plt.close('all')
+
+
+def plot_roc(y, y_hat, save_name, title_str):
+
+    fpr, tpr, roc_thresholds = roc_curve(y, y_hat)
+    roc_auc = roc_auc_score(y, y_hat)
+
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title_str)
+    plt.legend(loc="lower right")
+    plt.savefig(save_name)
+
+    plt.close('all')
+    return roc_auc
+
+
+def plot_precision_recall(y, y_hat, save_name, title_str):
+    precision, recall, pr_thresholds = precision_recall_curve(y, y_hat)
+    ap_score = average_precision_score(y, y_hat)
+
+    plt.figure()
+    plt.plot(recall, precision, label='Precision-Recall AUC={0:0.2f}'.format(ap_score))
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.0])
+    plt.xlim([0.0, 1.0])
+    plt.title(title_str)
+    plt.legend(loc="lower left")
+    plt.savefig(save_name)
+
+    plt.close()
+    return ap_score
